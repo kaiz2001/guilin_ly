b36502ff006c642299ef0591cf5c2e5bf3a134dc
Moves <1423287744@qq.com>
2023-03-31 20:16:22 +0800

    第一次提交

---
+++ script.js
@@ -0,0 +1,8 @@
+const wzgl = document.getElementById('wzgl');
+const home = document.getElementById('home');
+wzgl.onclick = function () {
+    location.href = "wzgl.html";
+}
+home.onclick = function () {
+    location.href = "index.html";
+}
